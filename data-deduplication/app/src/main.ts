d7bd0aba7a269c8cc13cf3e151e267785505d78c
Andranik Arakelyan <andranik.arakelyan.me@gmail.com>
2025-08-27 09:15:04 +0400

    chore: initial server setup

--- data-deduplication/app/src/main.ts
+++ data-deduplication/app/src/main.ts
@@ -1,1 +1,10 @@
-console.log('hello world');
+import { startServer } from "./server";
+
+(async () => {
+    try {
+        await startServer();
+    } catch (error) {
+        console.error("Failed to start an application:", error);
+        process.exit(1);
+    }
+})();
